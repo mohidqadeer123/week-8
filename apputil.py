c02cbf51e553304a511a3b1e0ded98dcaa9796cc
mohidqadeer123 <mohidqadeer@iu.edu>
2025-10-22 23:57:01 +0500

    term dict fucntion

--- apputil.py
+++ apputil.py
@@ -2,10 +2,16 @@ from collections import defaultdict
 
 
 class MarkovText(object):
-
+    '''Building a class MarkovText that includes functions to generate a dictionary containing key value pairs,
+      where the keys represent tokens (words) and the values represent their possible subsequent states within a corpus.
+    '''
     def __init__(self, corpus):
+
+        '''Creating a function that assigns a corpus and ensures that each key 
+           in the resulting dictionary is automatically initialized with a list as its value.
+        '''
         self.corpus = corpus
-        self.term_dict = None  # you'll need to build this
+        self.term_dict = defaultdict(list)
 
     def get_term_dict(self):
 
